3c68272686dec624bb06107ab9b0f377ab1ec83a
TinyProbe <137adv167@gmail.com>
2022-10-24 23:06:55 +0900

    Update cmp.sh

--- cmp.sh
+++ cmp.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-g++ -Wall -Wextra -Werror -D TINY___ sol1.cpp -o sol1
-g++ -Wall -Wextra -Werror -D TINY___ sol2.cpp -o sol2
-./sol1
-./sol2
+g++ -Wall -Wextra -Werror -D TINY__ sol1.cpp -o sol1
+g++ -Wall -Wextra -Werror -D TINY__ sol2.cpp -o sol2
+./sol1 > out1.txt
+./sol2 > out2.txt
 diff ./out1.txt ./out2.txt
 rm ./sol1 ./sol2 ./out1.txt ./out2.txt
