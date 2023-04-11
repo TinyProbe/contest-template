32d8274f21ef513f1e14769f3c8053b9d1467081
TinyProbe <137adv167@gmail.com>
2023-04-11 11:36:19 +0900

    update

--- sol1.cpp
+++ sol1.cpp
@@ -2,14 +2,14 @@
 #define int long long
 #define F first
 #define S second
-#define TIEIO cin.tie(0)->ios::sync_with_stdio(0)
-#define TESTCASE(N) int t; for (N ? t=N : bool(cin>>t); t>0; --t) solve()
-#define SOLUTION(N)\
+#define untieio cin.tie(0)->ios::sync_with_stdio(0)
+#define testcase(N) int t; for (N ? t=N : bool(cin>>t); t>0; --t) solve()
+#define solution(N)\
 	void solve();\
-	signed main() { TIEIO; TESTCASE(N); assert(cin && !(cin >> t)); }\
+	signed main() { untieio; testcase(N); assert(cin && !(cin>>t)); }\
 	void solve()
 using namespace std;
 
-SOLUTION(0) {
-	
+solution(0) {
+
 }
--- sol2.cpp
+++ sol2.cpp
@@ -84,22 +84,13 @@ static FastOut fout;
 #define int long long
 #define F first
 #define S second
-#define TIEIO cin.tie(0)->ios::sync_with_stdio(0)
-#define TESTCASE(N) int t; for (N ? t=N : fin>>t; t>0; --t) solve()
-#define SOLUTION(N)\
+#define untieio cin.tie(0)->ios::sync_with_stdio(0)
+#define testcase(N) int t; for (N ? t=N : bool(cin>>t); t>0; --t) solve()
+#define solution(N)\
 	void solve();\
-	signed main() { TIEIO; TESTCASE(N); assert(fin && !(fin >> t)); }\
+	signed main() { untieio; testcase(N); assert(cin && !(cin>>t)); }\
 	void solve()
-#ifdef __TINY__
-using namespace chrono;
-class TimeTracker {
-	time_point<system_clock> b, e;
-public:
-	TimeTracker() { b = system_clock::now(); }
-	~TimeTracker() { e = system_clock::now(); fout << '\n' << microseconds(e - b).count() << " us\n"; }
-} time_tracker__;
-#endif
 
-SOLUTION(0) {
-	
+solution(0) {
+
 }
