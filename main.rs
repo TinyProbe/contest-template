850aa4e49f2f336cd7c8f8e1184cb2c7475b60c1
TinyProbe <137adv167@gmail.com>
2023-07-19 19:12:45 +0900

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,5 +1,5 @@
 #![allow(dead_code, unused)]
-
+ 
 use std::io::*;
 use std::mem::swap;
 use std::cmp::{min, max};
@@ -19,19 +19,51 @@ fn scan<T: std::str::FromStr>()->T {
     }
 }
 fn bufw()->&'static mut BufWriter<Stdout> { // '
-    static mut WR: Option<BufWriter<Stdout>> = None;
+    static mut BUF: Option<BufWriter<Stdout>> = None;
     unsafe {
-        if let None = WR { WR = Some(BufWriter::new(stdout())); }
-        return WR.as_mut().unwrap();
+        if let None = BUF { BUF = Some(BufWriter::new(stdout())); }
+        return BUF.as_mut().unwrap();
     }
 }
+macro_rules! input {
+    ()=>{};()=>[];
+    (mut $var:ident: $t:tt, $($rest:tt)*)=>{input!(mut $var: $t);input!($($rest)*);};
+    (mut $var:ident: $t:tt)=>{let mut $var = __input_inner!($t);};
+    ($var:ident: $t:tt, $($rest:tt)*)=>{input!($var: $t);input!($($rest)*);};
+    ($var:ident: $t:tt)=>{let $var = __input_inner!($t);};
+    (($($var:tt)*): ($($t:tt),*), $($rest:tt)*)=>{
+        let ($($var)*) = __input_inner!(($($t),*));input!($($rest)*);
+    };
+    (($($var:tt)*): ($($t:tt),*))=>{ let ($($var)*) = __input_inner!(($($t),*)); };
+}
+macro_rules! __input_inner {
+    (($($t:tt),*)) => {($(__input_inner!($t)),*)};
+    ([$t:tt; $n:expr])=>{(0..$n).map(|_| __input_inner!($t)).collect::<Vec<_>>()};
+    (chars)=>{__input_inner!(String).chars().collect::<Vec<_>>()};
+    (vytes)=>{__input_inner!(String).bytes().collect::<Vec<_>>()};
+    (bytes)=>{__input_inner!(String).into_bytes()};
+    (usize1)=>{__input_inner!(usize)-1};
+    ($t:ty)=>{scan::<$t>()};
+}
+macro_rules! u8 {($n:expr)=>(($n) as u8)}
+macro_rules! u16 {($n:expr)=>(($n) as u16)}
+macro_rules! u32 {($n:expr)=>(($n) as u32)}
+macro_rules! u64 {($n:expr)=>(($n) as u64)}
+macro_rules! u128 {($n:expr)=>(($n) as u128)}
+macro_rules! usize {($n:expr)=>(($n) as usize)}
+macro_rules! i8 {($n:expr)=>(($n) as i8)}
+macro_rules! i16 {($n:expr)=>(($n) as i16)}
+macro_rules! i32 {($n:expr)=>(($n) as i32)}
+macro_rules! i64 {($n:expr)=>(($n) as i64)}
+macro_rules! i128 {($n:expr)=>(($n) as i128)}
+macro_rules! isize {($n:expr)=>(($n) as isize)}
+macro_rules! bool {($n:expr)=>(($n) as bool)}
 macro_rules! print {($($fmt:tt)*)=>(write!(bufw(),$($fmt)*))}
 macro_rules! println {($($fmt:tt)*)=>(writeln!(bufw(),$($fmt)*))}
 macro_rules! ternary {($cdt:expr;$true:expr;$false:expr)=>(if$cdt{$true}else{$false})}
 macro_rules! testcase {($n:expr)=>(for _ in 0..ternary!(($n)!=0;($n);scan()){solve();}bufw().flush().unwrap())}
-
+ 
 fn main() { testcase!(0); }
 fn solve() {
     
 }
-
