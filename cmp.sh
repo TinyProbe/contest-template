86c90bf87e4652c9e540b205b5bf6d5664c28b47
TinyProbe <137adv167@gmail.com>
2023-09-18 22:18:13 +0900

    Update cmp.sh

--- cmp.sh
+++ cmp.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-g++ --std=c++20 -Wall -Wextra -Werror -O2 -D _GLIBCXX_HOSTED -D _GLIBCXX_NO_ASSERT sol1.cpp -o sol1
-g++ --std=c++20 -Wall -Wextra -Werror -O2 -D _GLIBCXX_HOSTED -D _GLIBCXX_NO_ASSERT sol2.cpp -o sol2
-./sol1 < in > out1
-./sol2 < in > out2
-diff out1 out2
-rm sol1 sol2 out1 out2
+c++ --std=c++20 -Wall -Wextra -Werror -O2 -D _GLIBCXX_HOSTED -D _GLIBCXX_NO_ASSERT sol1.cpp -o sol1
+c++ --std=c++20 -Wall -Wextra -Werror -O2 -D _GLIBCXX_HOSTED -D _GLIBCXX_NO_ASSERT sol2.cpp -o sol2
+./sol1 < in.txt > out1.txt
+./sol2 < in.txt > out2.txt
+diff out1.txt out2.txt
+rm sol1 sol2 out1.txt out2.txt
