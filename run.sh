85352fa43c19922bf23d752614fa322a7a65ef26
TinyProbe <137adv167@gmail.com>
2023-10-11 12:16:02 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-c++ --std=c++20 -Wall -Wextra -Werror -O2 -D _GLIBCXX_HOSTED -D _GLIBCXX_NO_ASSERT -D __TINY__ sol1.cpp -o sol1
+c++ --std=c++20 -Wall -Wextra -Werror -g3 -D _GLIBCXX_HOSTED -D _GLIBCXX_NO_ASSERT sol1.cpp -o sol1
 ./sol1 < in.txt
-rm sol1
+rm -rf sol1 sol1.dSYM
