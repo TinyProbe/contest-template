4f4e69298a63a6ddcbf4653ae378cd9a27e85aac
TinyProbe <137adv167@gmail.com>
2024-01-04 22:55:09 +0900

    Update sol1.cpp

--- sol1.cpp
+++ sol1.cpp
@@ -1,15 +1,14 @@
 #include <bits/stdc++.h>
-using namespace std;
 #define int int64_t
 #define endl '\n'
-#define Int (cin >> g_i, g_i)
-#define Str (cin >> g_s, g_s)
-int g_i; string g_s;
+using namespace std;
 
 void solve() {
 }
 
 int32_t main() {
   ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-  for (int i = (0 ? 1 : Int); i; --i) { solve(); }
+  int t = 0;
+  if (t == 0) { cin >> t; }
+  while (t--) { solve(); }
 }
