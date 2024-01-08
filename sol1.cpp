d42bc95ba78ca14b3369ddbff8b121747e57394e
TinyProbe <137adv167@gmail.com>
2024-01-09 01:13:37 +0900

    Update sol1.cpp

--- sol1.cpp
+++ sol1.cpp
@@ -3,11 +3,12 @@
 #define endl '\n'
 using namespace std;
 
-int T(0);
 void solve() {
 }
 
 int32_t main() {
+  int T(0);
+
   ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
   for (T || cin >> T; T--; ) { solve(); }
 }
