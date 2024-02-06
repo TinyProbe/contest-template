ff00e0774049c82b419f29863b6986a3d99f0cad
TinyProbe <137adv167@gmail.com>
2024-02-06 10:59:52 +0900

    Update sol1.cpp

--- sol1.cpp
+++ sol1.cpp
@@ -1,14 +1,13 @@
 #include <bits/stdc++.h>
-#define int int64_t
-#define endl '\n'
 using namespace std;
 
 void solve() {
 }
 
-int32_t main() {
-  int T = 0;
-
+int main() {
   ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-  for (T || cin >> T; T--; ) { solve(); }
+  cout << fixed << setprecision(20);
+  int T = 1;
+  cin >> T;
+  while (T--) { solve(); }
 }
