e155e452379f2048f56615980d89fe8f434fc850
TinyProbe <137adv167@gmail.com>
2024-07-16 23:27:38 +0900

    Update main.cc

--- main.cc
+++ main.cc
@@ -54,6 +54,8 @@ void solve() {
 }
 
 int main() {
+  ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+  cout << fixed << setprecision(20);
   usize t = 1;
   // scan >> t;
   while (t--) { solve(); }
