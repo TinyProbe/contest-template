7bd5326d6a01ddbde7ffb26913305e7d008a88ac
TinyProbe <137adv167@gmail.com>
2024-07-17 11:59:55 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,13 +4,19 @@
 #include <string.h>
 #include <math.h>
 
-#define min(a, b) ((a) < (b) ? (a) : (b))
-#define max(a, b) ((a) > (b) ? (a) : (b))
+#define min(a, b) ((a)<(b) ? (a) : (b))
+#define max(a, b) ((a)>(b) ? (a) : (b))
+#define swap(a, b) ((a)^=(b),(b)^=(a),(a)^=(b))
 #define true  (u8)1
 #define false (u8)0
 #define null  (void *)0
-#define getc  getchar_unlocked
-#define putc  putchar_unlocked
+#if (__STDC_VERSION__ >= 201112L)
+# define getc getchar
+# define putc putchar
+#else
+# define getc getchar_unlocked
+# define putc putchar_unlocked
+#endif
 
 typedef unsigned char       u8;
 typedef unsigned short      u16;
@@ -29,41 +35,40 @@ typedef u8                  bool;
 int getchar_unlocked(void);
 int putchar_unlocked(int c);
 
-i8 _buff[1 << 5];
+i8 _io[1 << 5];
 
 bool _isspace(i8 c) { return (c>=9 && c<=13) || c==' '; }
 bool _isdigit(i8 c) { return c>='0' && c<='9'; }
-void reads(i8 *s) { // no check range.
-  *s = getc();
-  while (*s!=-1 && _isspace(*s)) { *s = getc(); }
-  while (*s!=-1 && !_isspace(*s)) { *++s = getc(); }
-  *s = '\0';
+usize reads(i8 *const s) { // no check memory.
+  usize   i = 0;
+  do { s[i] = getc(); } while (s[i]!=-1 && _isspace(s[i]));
+  while (s[i]!=-1 && !_isspace(s[i])) { s[++i] = getc(); }
+  s[i] = '\0';
+  return i;
 }
 i64 readi(void) {
   usize   i = 0;
   i64     res = 0;
   bool    pos = true;
-  reads(_buff);
-  if (_buff[i]=='-' || _buff[i]=='+') { pos = _buff[i++]=='+'; }
-  while (_buff[i] && _isdigit(_buff[i])) {
-    res = (res<<1) + (res<<3) + (_buff[i++]^'0');
-  }
+  reads(_io);
+  if (_io[i]=='-' || _io[i]=='+') { pos = _io[i++]=='+'; }
+  while (_io[i] && _isdigit(_io[i]))
+    res = (res<<1) + (res<<3) + (_io[i++]^'0');
   return pos ? res : -res;
 }
 void writes(i8 const *s) { while (*s) { putc(*(s++)); } }
 void writei(i64 n) {
   usize   i = (1 << 5) - 1;
   if (!n) { putc('0'); return; }
-  _buff[i] = '\0';
-  while (n) {
-    _buff[--i] = (n % 10 + '0'), n /= 10;
-  }
-  writes(_buff + i);
+  _io[i] = '\0';
+  while (n)
+    _io[--i] = (n % 10 + '0'), n /= 10;
+  writes(_io + i);
 }
 
 void solve(void); int main(void) {
   usize t = 1;
-  // t = readi();
+  t = readi();
   while (t--) { solve(); }
   return 0;
 }
