4001a7fb6d7df716a53f8cf8e3f567f04f58978f
TinyProbe <137adv167@gmail.com>
2024-08-01 02:12:44 +0900

    Update main.cc

--- main.cc
+++ main.cc
@@ -13,30 +13,32 @@ using isize = long;
 using f32 = float;
 using f64 = double;
 
+#define BUFF_SIZE (1 << 5)
+#define _isspace(c) (((c)>=9 && (c)<=13) || (c)==' ')
+#define _isdigit(c) ((c)>='0' && (c)<='9')
+
 class Scanner {
-  static constexpr usize BUFFER_SIZE = 1 << 22; // warning: string cutting.
-  i8 buffer[BUFFER_SIZE];
+  i8 buff[BUFF_SIZE];
   usize len, cur;
 
  public:
-  Scanner() : len(fread(buffer, sizeof(i8), BUFFER_SIZE, stdin)), cur() {}
-  inline bool _isspace(i8 c) { return (c>=9 && c<=13) || c==' '; }
-  inline bool _isdigit(i8 c) { return c>='0' && c<='9'; }
+  Scanner() : len(), cur() {}
+  inline i8 const *nextItem() { // no check memory.
+    usize   i = 0;
+    do { buff[i] = getchar(); } while (buff[i]!=-1 && _isspace(buff[i]));
+    while (buff[i]!=-1 && !_isspace(buff[i])) { buff[++i] = getchar(); }
+    buff[i] = '\0';
+    return buff;
+  }
   inline i64 _atoll(i8 const *s) {
-    i64 res = 0;
-    bool pos = true;
-    if (*s=='-' || *s=='+') { pos = *(s++)=='+'; }
-    while (*s && _isdigit(*s)) {
-      res = (res<<1) + (res<<3) + (*(s++)^'0');
+    usize   i = 0;
+    bool    pos = true;
+    i64     res = 0;
+    if (s[i]=='-' || s[i]=='+') { pos = s[i++]=='+'; }
+    while (_isdigit(s[i])) {
+      res = (res<<1) + (res<<3) + (s[i++]^'0');
     }
     return pos ? res : -res;
-  }
-  inline i8 const *nextItem() {
-    while (cur < len && _isspace(buffer[cur])) { ++cur; }
-    usize l = cur;
-    while (cur < len && !_isspace(buffer[cur])) { ++cur; }
-    buffer[cur++] = '\0';
-    return buffer + l;
   }
   inline Scanner &operator>>(u8 &ref) { ref = _atoll(nextItem()); return *this; }
   inline Scanner &operator>>(u16 &ref) { ref = _atoll(nextItem()); return *this; }
@@ -57,7 +59,7 @@ void solve(); int main() {
   ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
   cout << fixed << setprecision(20);
   usize t = 1;
-  // scan >> t;
+  scan >> t;
   while (t--) { solve(); }
 }
 
