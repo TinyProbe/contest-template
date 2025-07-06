025191bb90cb73fcd678ad9bea8d4e406c800692
TinyProbe <137adv167@gmail.com>
2025-07-07 01:56:17 +0900

    Update main.c

--- main.c
+++ main.c
@@ -10,7 +10,7 @@
 #define min(a, b)   ((a)<(b) ? (a) : (b))
 #define max(a, b)   ((a)>(b) ? (a) : (b))
 #define swap(a, b)  ((a)^=(b),(b)^=(a),(a)^=(b))
-#define permusum(a) (((a)*(a)+(a))/2)
+#define psum(a)     (((a)*(a)+(a))/2)
 #define loop(var, init, end) \
     for (int var = (init), __end = (end), __asc = var < __end; \
             __asc ? var < __end + 1 : var > __end - 1; \
