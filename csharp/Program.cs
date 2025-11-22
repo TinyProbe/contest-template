0de02d055562ba38e94cb9a660fb824998a9678a
TinyProbe <137adv167@gmail.com>
2025-11-22 18:58:44 +0900

    Update Changes

---
+++ csharp/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace App;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        int tc = 1;
+        // tc = Console.Read();
+        while (tc-- > 0)
+        {
+            Solve();
+        }
+    }
+
+    private static void Solve()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ csharp/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+dotnet run < in.txt > out.txt
